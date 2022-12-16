aa31e8d0e247c1de48909605245d5734207757cc
VidyaChandrub <vidyac266@gmail.com>
2022-12-16 20:04:22 +0530

    contact

--- contact-app/src/index.js
+++ contact-app/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './Components/App';
+import App from './App';
+/* font awesom */
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
+
+/* bootsrap */
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
