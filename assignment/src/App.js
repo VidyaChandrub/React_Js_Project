fe0a07fe54590dcd718e540ea8c6b304334d3f47
VidyaChandrub <vidyac266@gmail.com>
2023-01-31 22:33:00 +0530

    appointment

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,54 +1,23 @@
+let App = () => {
 
-import "./App.css";
-import React, { useState } from "react";
-import JsonData from "./MOCK_DATA.json";
-import ReactPaginate from "react-paginate";
-import Viewdata from "./Viewdata";
+return <>
+<br/>
+<div className="row">
+  <div className="col-md-4">
+    <label>Date *</label>
+    <input type="date" />
+  </div>
+  <div  className="col-md-4">
+  <label>Date *</label>
+    <input type="time" />
+  </div>
+  <div  className="col-md-4">
+    <input type="text" />
+  </div>
+  <input type="submit" value="Add" className='btn btn-primary' />
 
-
-function App() {
-  const [users, setUsers] = useState(JsonData); //JsonData.slice(0, 50)
-  const [pageNumber, setPageNumber] = useState(0);
-
-  const usersPerPage = 10;
-  const pagesVisited = pageNumber * usersPerPage;
-
-  const displayUsers = users
-    .slice(pagesVisited, pagesVisited + usersPerPage)
-    .map((user) => {
-      return (
-        <div className="user">
-          <h3>{user.name}</h3>
-           <h3>{user.email}</h3>
-          <h3>{user.company.name}</h3> 
-           <a href="/viewdata" ><button  className={"paginationActive"}><Viewdata/></button></a>
-        </div>
-      );
-    });
-
-  const pageCount = Math.ceil(users.length / usersPerPage);
-
-  const changePage = ({ selected }) => {
-    setPageNumber(selected);
-  };
-
-  return (
-    <div className="App">
-      {displayUsers}
-      
-      <ReactPaginate
-        previousLabel= {<img src="https://upload.wikimedia.org/wikipedia/commons/7/74/Less_than_symbol.jpg" height="10px" width="10px"/>}
-        nextLabel={<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz7iRlbHfDpBKdThku5oPbyKbvMpFUaX4FdkuWTWnZjtjlWuUGSbXuMgMSpQfsVKQnrEY&usqp=CAU" height="15px" width="10px"/>}
-        pageCount={pageCount}
-        onPageChange={changePage}
-        containerClassName={"paginationBttns"}
-        previousLinkClassName={"previousBttn"}
-        nextLinkClassName={"nextBttn"}
-        disabledClassName={"paginationDisabled"}
-        activeClassName={"paginationActive"}
-      />
-    </div>
-  );
+</div>
+</>
 }
 
 export default App;
--- assignment/src/Viewdata.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Viewdata = () => {
-  return (
-    <div>Viewdata</div>
-  )
-}
-
-export default Viewdata
